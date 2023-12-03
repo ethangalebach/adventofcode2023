fa235ac3f1163e973edb09536f31d6740f6d9d5c
Ethan Galebach <ethan.galebach@gmail.com>
2023-12-03 13:29:06 -0800

    day01 pt2

--- src/day01.py
+++ src/day01.py
@@ -1,41 +1,64 @@
 import utils
+import time
+from multiprocessing import Pool
+from math import inf
 
-def get_calibration_value(line:str) -> int:
-    for c in line:
-        if c.isdigit():
-            first_digit = int(c)
-            break
-    for c in reversed(line):
-        if c.isdigit():
-            return first_digit*10 + int(c)
+def get_pt1_calibration_value(line:str) -> int:
+    first_digit = int(next(c for c in line if c.isdigit()))
+    last_digit = int(next(c for c in line[::-1] if c.isdigit()))
+    return first_digit*10 + last_digit
 
-def get_calibration_sum(input_path:str) -> int:
-    cal_sum = 0
+def get_pt2_calibration_value(line:str) -> int:
+    digit_words = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    digit_chars = ['0','1','2','3','4','5','6','7','8','9']
+    first_digit = last_digit = None
+    first_idx = last_idx = inf
+    for i, s in enumerate(digit_words + digit_chars):
+        if s in line:
+            if line.index(s) < first_idx:
+                first_idx = line.index(s)
+                first_digit = i % 10
+            if line[::-1].index(s[::-1]) < last_idx:
+                last_idx = line[::-1].index(s[::-1])
+                last_digit = i % 10
+    return first_digit*10 + last_digit
+
+def parallel_apply(func, input_path:str) -> int:
     with open(input_path) as f:
-        for line in f:
-            if line.strip():
-                cal_sum += get_calibration_value(line)
-    return cal_sum
+        lines = [line for line in f if line.strip()]
+    with Pool() as pool:
+        results = pool.map(func, lines)
+    return sum(results)
 
 def get_answer(input_path: str, part: int) -> int:
     if part == 1:
-        return get_calibration_sum(input_path)
+        return parallel_apply(get_pt1_calibration_value, input_path)
     elif part == 2:
-        return (len(map.get_path_with_ideal_trailhead()))
+        return parallel_apply(get_pt2_calibration_value, input_path)
     else:
         raise Exception('not part 1 or 2')
 
 if __name__ == "__main__":
     input_path = utils.get_input_path(__file__)
-    test_path = utils.get_test_path(__file__)
-    test_answer = get_answer(test_path, part=1)
-    assert test_answer == 142, (
-        f"got calibration sum of {test_answer}, should be 142"
+    pt1_test_path = utils.get_test_path(__file__, 1)
+    pt2_test_path = utils.get_test_path(__file__, 2)
+    pt1_test_answer = get_answer(pt1_test_path, part=1)
+    assert pt1_test_answer == 142, (
+        f"got calibration sum of {pt1_test_answer}, should be 142"
     )
+    pt2_test_answer = get_answer(pt2_test_path, part=2)
+    assert pt2_test_answer == 281, (
+        f"got calibration sum of {pt2_test_answer}, should be 281"
+    )
+    pt1_start_time = time.time()
     part1_answer = get_answer(input_path, part=1)
-    # part2_answer = get_answer(input_path, part=2)
+    pt1_end_time = pt2_start_time = time.time()
+    part2_answer = get_answer(input_path, part=2)
+    pt2_end_time = time.time()
+    pt1_duration = 1000*(pt1_end_time - pt1_start_time)
+    pt2_duration = 1000*(pt2_end_time - pt2_start_time)
 
     print(
-        f"Part 1 Answer: {part1_answer},",
-        # f"Part 2 Answer: {part2_answer}"
+        f"Part 1 Answer: {part1_answer}, in {pt1_duration} ms\n",
+        f"Part 2 Answer: {part2_answer}, in {pt2_duration} ms"
     )
--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,8 @@ def get_input_path(file):
     file_dir = os.path.dirname(os.path.dirname(file))
     return os.path.join(file_dir, f'input/{filename}')
 
-def get_test_path(file):
+def get_test_path(file, part):
     filename = os.path.basename(file)
-    filename = filename.removesuffix('.py') + '.txt'
+    filename = filename.removesuffix('.py') + f'_pt{part}.txt'
     file_dir = os.path.dirname(os.path.dirname(file))
     return os.path.join(file_dir, f'tests/test_{filename}')
